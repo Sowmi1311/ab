913c2e0b07a0b3952142cf3a3eb4fecf4e7459c1
hp <hp@DESKTOP-RUPR7S2>
2023-10-08 12:39:25 +0530

    added one more "s"

--- com.project.employee/com.project.employee/src/main/java/com/project/employee/controller/Controller.java
+++ com.project.employee/com.project.employee/src/main/java/com/project/employee/controller/Controller.java
@@ -14,7 +14,7 @@ import com.project.employee.model.Employee;
 import com.project.employee.repository.EmpRepository;
 
 @RestController
-@RequestMapping("/homes")
+@RequestMapping("/homess")
 public class Controller {
 	
 	
